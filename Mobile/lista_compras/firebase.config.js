d87bd52452bf3f7e3b4a7aa787326cd6b1f5a8db
bertuci <g.davila@aluno.senai.brgit config --global user.email g.davila@aluno.senai.br>
2026-05-11 12:50:48 -0300

    aula douglas manha 11/05

--- Mobile/lista_compras/firebase.config.js
+++ Mobile/lista_compras/firebase.config.js
@@ -1,12 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBP4dhsYjEaBtV0sAPSdi8vz5kWpqtxCzc",
   authDomain: "listacompras-58bc8.firebaseapp.com",
@@ -20,3 +17,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+export const firestore = getFirestore(app);
--- Mobile/lista_compras/src/ListaCompras.js
+++ Mobile/lista_compras/src/ListaCompras.js
@@ -1,35 +1,65 @@
-import { useState } from "react";
+import { useState, useEffect, use } from "react";
 import { Text, View, TextInput, TouchableOpacity, FlatList, Image, StatusBar } from "react-native";
 import Estilos, { corPrincipal, corSecundaria, corTexto, corFundo, corFundo2, corPlaceholder } from "./Estilos";
 import { MaterialIcons } from '@expo/vector-icons';
+import { firestore } from "../firebase.config";
+import { collection, addDoc, getDocs, query, doc, updateDoc, deleteDoc, where, orderBy } from "firebase/firestore";
 
 const ListaCompras = () => {
     // variavel de estado para o item que irei adicionar a minha lista de compras
     const [item, setItem] = useState('');
-    const [listaCompras, setListaCompras] = useState([
-        { id: 1, produto: '1 Cartela de Ovos 🥚', comprado: false },
-        { id: 2, produto: '2 Nikito de Chocolate🍪', comprado: true }
-    ]);
+    const [listaCompras, setListaCompras] = useState([]);
+
+    async function buscarDados() {
+        // representa um SELECT * FROM COMPRAS
+        const comando = query(collection(firestore, 'compras'));
+        const dadosBD = await getDocs(comando);
+
+        const novaLista = dadosBD.docs.map( (doc) => (
+            {id: doc.id, ...doc.data()}
+        ))
+
+        setListaCompras(novaLista);
+    }
+
+    useEffect(() => {
+        buscarDados();
+    }, []);
+
+    async function botaoAtualizar(item) {
+        const docRef = doc(firestore, 'compras', item.id);
+        await updateDoc(docRef, { comprado: !item.comprado });
+        buscarDados();
+    }
+
+
+    async function botaoExcluir(id) {
+        await deleteDoc(doc(firestore, 'compras', id));
+        buscarDados();
+    }
+
 
     // funçao para desenhar os itens da minha lista de compras
     const exibirItens = ({ item }) => {
         return (
-            <TouchableOpacity style={Estilos.botaoItem}>
-                <Text style={Estilos.textoBotaoItem}>{item.produto}</Text>
-                <MaterialIcons name='delete-outline' size={24} color={corPrincipal} />
+            <TouchableOpacity style={Estilos.botaoItem} onPress={() => botaoAtualizar(item)} >
+                <Text style={item.comprado == false ? Estilos.textoBotaoItem : Estilos.textoBotaoItemComprado}>
+                    {item.produto}
+                </Text>
+                <MaterialIcons name='delete-outline' size={24} color={corPrincipal} onPress={() => botaoExcluir(item.id)} />
             </TouchableOpacity>
         )
     }
 
-    const botaoAdicionar = () => {
+    const botaoAdicionar = async () => {
         // criando um novo objeto do produto que estou adicionando
-        const novoItem = { id: Date.now(), produto: item, comprado: false }
-        // criando uma nova lista, mantendo tudo que tenho da lista atual e acrescentando o novo item
-        const novaLista = [...listaCompras, novoItem];
-        // atribuo a nova lista a lista de compras que estou exibindo no app
-        setListaCompras(novaLista);
+        const novoItem = { produto: item, comprado: false }
+        const docRef = await addDoc(collection(firestore, 'compras'), novoItem);
+        console.log(docRef);
+
         // limpando o campo de input
         setItem('');
+        buscarDados();
     }
 
     return (
